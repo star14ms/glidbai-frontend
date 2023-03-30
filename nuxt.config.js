3a5f47dd77f1df324fa2eb3bb0c5a7c01fa48b5e
star14ms <68639278+star14ms@users.noreply.github.com>
2023-03-30 22:43:05 +0900

    Feat: Add global CSS

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/styles/main.scss', 
+    '~/assets/fonts/main.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
