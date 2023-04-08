0ad060232a6d7200a1b24c16374aaa7b8eedbb7d
star14ms <68639278+star14ms@users.noreply.github.com>
2023-04-08 22:56:49 +0900

    Refac: Restrict access to question pages in specific situations

--- src/middleware/question/_id.js
+++ src/middleware/question/_id.js
@@ -1,8 +1,7 @@
 export default function ({ route, redirect, store }) {
-    if (
-        store.state.user.userCurriculum.length === 0 || 
-        route.params.id <= 0 || store.state.OMR.n_question < route.params.id
-    ) {
+    if (store.state.user.userCurriculum.length === 0) {
         redirect('/')
+    } else if (route.params.id <= 0 || store.state.OMR.n_question < route.params.id) {
+        redirect('/question/1')
     }
 }
